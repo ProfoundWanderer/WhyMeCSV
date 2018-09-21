a841cf8fa1d06ebbf47518b15321e7a2c66e38f4
ProfoundWanderer <18703678+ProfoundWanderer@users.noreply.github.com>
2018-09-21 13:03:41 -0500

    Lots of changes
    Made it a little cleaner. Things move around now and stuff

--- CleanUp.py
+++ CleanUp.py
@@ -1,29 +1,63 @@
 import pandas as pd
 import numpy as np
+import re
+
+"""
+Current Issues:
+
+"""
+
 
 # point to file location.
-filename = 'C:/Users/Free/Desktop/try_combine_rows.csv'
+filename = '/Users/derrick/Documents/Random Stuff/CSV/try_combine_rows.csv'
 df = pd.read_csv(filename)
 df.columns = [i.lower().replace(' ', '_') for i in df.columns]  # lower case and replace spaces
 df.index += 2  # so when it says "check these lines" the numbers match with csv
+df = df.dropna(how='all') # removes rows that are completely empty
 
 
+# Checks for duplicate emails
 email_list = df['email']
 email_counts = email_list.value_counts()
 duplicate_emails = list(email_counts[email_counts > 1].index)
 print(f'Check these emails for duplicates: {duplicate_emails}')
 
-df.rename(columns={'direct_no': 'phone'}, inplace=True)
 
-# Next goal is to pull list of phone numbers from row 3 that have less than 8 digits
+# Prints the lines where phone len is less than 8 or greater than 15
 phone_list = df['phone']
 phone_list = phone_list.replace('[^0-9]+', '', regex=True)  # remove special characters
-phone_len = phone_list.str.len()
-phone_bad = list(phone_len[(phone_len > 0) & (phone_len < 8)].index)
+phone_len = phone_list.astype(str).str.len() # had to add .astype(str) before .str in order for certain csvs to work
+phone_bad = list(phone_len[(phone_len > 0) & (phone_len < 8) | (phone_len > 15)].index)
 print(f'Check these lines for an incomplete phone number: {phone_bad}')
 
-get_col_headers = str(list(df[2:]))
+
+# Order columns so if rows needs to merge it is done properly
+cols = list(df)
+
+if 'first_name' in cols:
+    cols.insert(0, cols.pop(cols.index('first_name')))
+else:
+    print('No first_name column. Exiting...')
+    exit()
+
+if 'last_name' in cols:
+    cols.insert(1, cols.pop(cols.index('last_name')))
+else:
+    print('No last_name column. Exiting...')
+    exit()
+
+if 'email' in cols:
+    cols.insert(2, cols.pop(cols.index('email')))
+else:
+    print('No email column. Exiting...')
+    exit()
+
+
+# needed for row merger
+df = df.ix[:, cols]
+get_col_headers = str(list(df[3:]))
 col_headers = eval(get_col_headers)
+
 
 if (email_list.str.contains(',')).any():
     # splits email list so that everything before comma is put in email and the rest into secondary_email
@@ -36,41 +70,37 @@ if (email_list.str.contains(',')).any():
         df['second_contact_email'] = df['second_contact_email'].astype(str) + ',' + df['secondary_email']
         df = df.iloc[:, :-1]  # drops last column so there isn't double 'second_contact_email'
 
+    df.rename(columns={'secondary_email': 'second_contact_email'}, inplace=True)
 else:
-    pass
+    if 'second_contact_email' not in df.columns:
+        df['second_contact_email'] = ''
 
 
-# Validate email: [^@]+@[^@]+\.[^@]+ (it has exactly one @ sign and at least one . after the @)
-# (^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$) (another regex that can be used to verify emails)
-df['email'] = df['email'].str.replace('^(.(?!([^@]+@[^@]+\.[^@]+)))*$', '')
-
+# validate email and move bad ones
+df['second_contact_email'] = df[~df['email'].str.contains(pat=r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', case=False, na=False)]['email']
+df['email'] = df[df['email'].str.contains(pat=r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', case=False, na=False)]['email']
 
 # this adds 'second_contact_phone' in case they do not have the column
 phone_value = ''
 if 'second_contact_phone' not in df:
     df.loc[:, 'second_contact_phone'] = phone_value
 
-
-# Finds all numbers in phone that are less than 8 digits and moves them
-invalid_number = df['phone'].str.findall('^.{,7}$|^.{16,}$')
-df['second_contact_phone'] = df['second_contact_phone'].astype(str) + ',' + invalid_number.astype(str)
-# remove the random stuff but keep commas
-df['second_contact_phone'] = df['second_contact_phone'].replace('[^0-9,,]', '', regex=True)
-
-
-# only keep numbers in phone column. without this sometimes the remove dup section below doesn't work 100%
+# only keep numbers in phone column
 df['phone'] = df['phone'].replace('[^0-9]+', '', regex=True)
-# Removes "invalid" phone numbers from phone column. Less than 7 and greater than 15 but doesn't move them
-df['phone'] = df['phone'].replace('^.{,7}$|^.{16,}$', '', regex=True)
 
-# removes random extra commas. probably could combine with line 58
-df['second_contact_phone'] = df['second_contact_phone'].replace('[,]', '', regex=True)
+# moves phone numbers less than 8 and greater than 15 digits then removes them from phone
+df['second_contact_phone'] = df[~df['phone'].astype(str).str.contains(pat=r'^(?:(?!^.{,7}$|^.{16,}$).)*$', case=False, na=False)]['phone']
+df['phone'] = df[df['phone'].astype(str).str.contains(pat=r'^(?:(?!^.{,7}$|^.{16,}$).)*$', case=False, na=False)]['phone']
+
+
+# needed for below merger. Gets column headers
+get_col_headers = str(list(df))
+# gets rid of [''] around header names
+col_headers = eval(get_col_headers)
 
 # searches column named email and drops duplicates but keeps the first one and merges data
 # https://github.com/khalido/notebooks/blob/master/pandas-dealing-with-dupes.ipynb
-
 df["first_dupe"] = df.duplicated("email", keep=False) & ~df.duplicated("email", keep="first")
-
 
 def combine_rows(row, key="email", cols_to_combine=col_headers[3:]):
     """takes in a row, looks at the key column
@@ -81,8 +111,11 @@ def combine_rows(row, key="email", cols_to_combine=col_headers[3:]):
         # making a df of dupes item
         dupes = df[df[key] == row[key]]
 
-        for i, dupe_row in dupes.iloc[1:].iterrows():   # skipping the first row, since thats our first_dupe
+        # skipping the first row, since thats our first_dupe
+        for i, dupe_row in dupes.iloc[1:].iterrows():
             for col in cols_to_combine:
+                dupe_row[col] = str(dupe_row[col])
+                row[col] = str(row[col])
                 row[col] += ", " + dupe_row[col]
         # make sure first_dupe doesn't get processed again
         row.first_dupe = False
@@ -92,13 +125,15 @@ def combine_rows(row, key="email", cols_to_combine=col_headers[3:]):
 df = df.apply(combine_rows, axis=1, result_type=None)
 df.drop_duplicates(subset=["email"], inplace=True)
 df.groupby('email').agg(lambda x: ", ".join(x)).reset_index()
-
-
-# this drops the "first_dupe" column from above fuction
 del df['first_dupe']
 
+# gets rid of random nan that pops up sometimes
+df = df.replace('(?:^|\W)nan(?:$|\W)', '', regex=True) #idk if still needed
 
-df.to_csv('C:/Users/Free/Desktop/Done.csv', index=False)
+
+# Convert names back from ex. first_name so system auto catches it
+df.columns = [i.title().replace('_', ' ') for i in df.columns]
+df.to_csv('/Users/derrick/Desktop/done.csv', index=False)
 print('CSV has been printed.')
 
 
