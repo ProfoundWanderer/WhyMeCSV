f2e82a85893a3930d4ef010a409e0b53b209bcee
ProfoundWanderer <18703678+ProfoundWanderer@users.noreply.github.com>
2018-10-08 14:07:52 -0500

    Has list used by WhyMeCSV to rename columns

---
+++ header_list.py
@@ -0,0 +1,36 @@
+first_name_list = [
+    'firstname',
+    'primary_firstname',
+    'lead_first_name',
+    ]
+
+last_name_list = [
+    'lastname',
+    'primary_lastname',
+    'lead_last_name',
+    ]
+
+email_list = [
+    'email_address',
+    'emailaddress',
+    'email_(personal)_#1',
+    'email_address_1',
+    'email_1',
+    'lead_email',
+    'emails',
+    ]
+
+phone_list = [
+    'mobile_phone',
+    'cell_phone',
+    'primary_mobile_phone',
+    'phone_(mobile)_#1',
+    'telephone1',
+    'phone_1',
+    'phone_number',
+    'lead_phone',
+    'home_phone',
+    'home_#',
+    'phone_numbers',
+    'phones',
+    ]
