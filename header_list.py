18ce7d614b216ff46a95f2cf8c78777a95c7cd22
ProfoundWanderer <18703678+ProfoundWanderer@users.noreply.github.com>
2018-10-14 23:17:18 -0500

    Added all columns to match

--- header_list.py
+++ header_list.py
@@ -1,6 +1,35 @@
-colss = ['first_name', 'last_name', 'email', 'phone']
+rename_list = [
+    'first_name',
+    'last_name',
+    'email',
+    'phone',
+    'type',
+    'assigned_agent',
+    'assigned_lender',
+    'alternate_phone',
+    'street_address',
+    'city',
+    'state',
+    'zip',
+    'fax',
+    'office_phone',
+    'second_contact_phone',
+    'status',
+    'timeframe',
+    'area',
+    'home_type',
+    'source_details',
+    'minimum_bedrooms',
+    'minimum_bathrooms',
+    'minimum_price',
+    'maximum_price',
+    'start_date',
+    'last_active_at',
+    'last_communication_at',
+    'average_property_price',
+    ]
     
-listt = [
+match_list = [
     [
     'firstname',
     'primary_firstname',
@@ -17,6 +46,7 @@ listt = [
     'email_1',
     'lead_email',
     'emails',
+    'subscriber_email',
     ], [
     'mobile_phone',
     'cell_phone',
@@ -25,10 +55,109 @@ listt = [
     'telephone1',
     'phone_1',
     'phone_number',
+    'phone(where_available)',
     'lead_phone',
     'home_phone',
     'home_#',
     'phone_numbers',
     'phones',
+    ], [
+    'contact_type',
+    'leadtype',
+    'buyer/seller',
+    'buyer_seller',
+    ], [
+    'agent',
+    'agentname',
+    'lead_assigned_to_name',
+    'realtor',
+    'agent_assigned',
+    'buyeragent',
+    'assigned_staff',
+    'lead_owner',
+    'agent_id',
+    ], [
+    'lender',
+    ], [
+    'home_phone',
+    'phone_2',
+    ], [
+    'address',
+    'address1',
+    'address_1',
+    'street_1',
+    'address_street_1',
+    ], [
+    'city',
+    'address_city',
+    'city_1',
+    ], [
+    'state',
+    'state_1',
+    'address_state',
+    ], [
+    'zip',
+    'zipcode',
+    'zip_1',
+    'address_zip',
+    ], [
+    'fax_number'
+    ], [
+    'work_phone'
+    ], [
+    'secondary_contact_phone',
+    ], [
+    'statusname',
+    'pipeline_status',
+    ], [
+    'timeframe',
+    'timeframetype',
+    'buying_timeframe',
+    ], [
+    'looking_in',
+    'areasearchedmost',
+    'location',
+    'favorite_city',
+    ], [
+    'averagepropertytypeviewed',
+    'property_type',
+    ], [
+    'source',
+    'lead_source',
+    'originalsource',
+    'source_page_name',
+    'sub_source',
+    ], [
+    'averagebedsviewed',
+    'bedrooms',
+    ], [
+    'averagebathsviewed',
+    'bathrooms',
+    ], [
+    'min_price_preference',
+    ], [
+    'max_price_preference',
+    ], [
+    'datecreated',
+    'register_date',
+    'registered',
+    'date_added',
+    'registerdate',
+    'contact_created_date',
+    'registration_date',
+    'creation_date',
+    'created_at',
+    ], [
+    'dateactivity',
+    'last_login',
+    'lastvisitdate',
+    ], [
+    'lastphonecall',
+    'last_contacted_at',
+    ], [
+    'average_price',
+    'averageprice',
+    'list_price',
+    'avglistingprice',
     ]
     ]
